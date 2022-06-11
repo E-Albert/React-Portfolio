99f14043110e66cfb18cf428f4881621e6f3b815
E-Albert <magicspoon123@gmail.com>
2022-06-10 23:06:39 -0400

    Added headshot of myself and background image to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Photo from "../assets/img/circularheadshot.png";
 
 const Sidebar = () => {
 
     return (
         <>
             <div className="sidebar">
+                <img src={Photo} alt="Emerson Albert"/>
                 <p> Hello, My name is Emerson. This is just the beginning of my bounce back</p>
             </div>
         
