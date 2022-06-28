04f58233da454afc449cbd3738a159e783d61886
E-Albert <magicspoon123@gmail.com>
2022-06-28 15:34:09 -0400

    Added project h1 to project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
     return (
         <div id="projectField">
 
-           
+            <h1 id='projectLogo'>Projects</h1>
             <div id="projectCardDisplay">
                 {myProjects.map((project, index) => (
                     <ProjectCard project={project} key={index} />
