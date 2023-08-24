2c8f71148b1e6bb5d8df430c2b468bd7a448158c
E-Albert <magicspoon123@gmail.com>
2023-08-24 14:56:43 -0400

    removed unnecessary projects

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -23,20 +23,6 @@ export const myProjects = [
     img: "https://i.imgur.com/WcMNR4M.png",
   },
   {
-    title: "Note Taker",
-    githubLink: "https://github.com/E-Albert/the-note-taker",
-    website: "https://morning-refuge-89449.herokuapp.com/",
-    description: "A simple app to take notes with.",
-    img: "https://i.imgur.com/uDQ0bxb.png",
-  },
-  {
-    title: "Password Generator",
-    githubLink: "https://github.com/E-Albert/ultimate-password-generator",
-    website: "https://e-albert.github.io/ultimate-password-generator/",
-    description: "Generates a password based on selected criteria.",
-    img: "https://i.imgur.com/94Qevr7.png",
-  },
-  {
     title: "Work Day Scheduler",
     githubLink: "https://github.com/E-Albert/the-work-day-scheduler",
     website: "https://e-albert.github.io/the-work-day-scheduler/",
